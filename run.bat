63803dc9d0c180b11a3ff148b8a6ae4bec67b533
SamFox <58928842+Meteorrrrr@users.noreply.github.com>
2022-05-13 14:26:46 +0800

    一个脚本来运行程序
    一个脚本来运行程序

---
+++ run.bat
@@ -0,0 +1,4 @@
+javac -encoding UTF-8 -d out -cp .;lib\algs4.jar src\proj\*.java src\proj\bhtree\*.java src\proj\math\*.java
+# java -cp out;lib\algs4.jar proj.Main < sample1.txt
+java -cp out;lib\algs4.jar proj.SimulationSystem
+pause
